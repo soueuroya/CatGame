77917156a2014e7fab83468ce8d15bdcdfcb16d4
Pestilence476 <pestilence476@gmail.com>
2024-09-18 11:41:16 -0500

    Changes needing second hand
    I fixed something that did not transfer when I had to fic the github.
    Also I added a script that could be used for respawning and invis frames when you get hit.
    Please review the //notes and let me know if their are any questions.

--- Assets/myScripts/HealthActual.cs
+++ Assets/myScripts/HealthActual.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthActual : MonoBehaviour
+
+// Please fix any names and let me know the scripts I need to create or add to for this to work
+
+public class HealthActual : MonoBehaviour 
 {
+
     public int maxHealth;
     public int currentHealth;
     public float blink;
     public float immuned;
     public float Player;
-    public Renderer modelRenderer1;
-    public Renderer modelRenderer2;
+    //public Renderer modelRenderer1; Wait for the art?
+    //public Renderer modelRenderer2;
     private float blinkTime = 0.1f;
     private float immunedTime;
 
@@ -21,7 +25,7 @@ public class HealthActual : MonoBehaviour
     public bool isDead = false;
 
     
-    void Start()
+    void Start() 
     {
         currentHealth = maxHealth;
 
@@ -29,11 +33,92 @@ public class HealthActual : MonoBehaviour
     }
 
     
-    void Update()
+    void Update() 
     {
-        
+        if (immundedTime > 0)
+        {
+    
+
+            immundedTime -= Time.deltaTime;
+            blinkTime -= Time.deltaTime;
+
+            if (blinkTime <= 0)
+            {
+                modelRenderer1.enabled = !modelRenderer1.enabled;
+                modelRenderer2.enabled = !modelRenderer2.enabled;
+
+                blinkTime = blink;
+            }
+            if (immundedTime <= 0)
+            {
+                modelRenderer1.enabled = true;
+                modelRenderer2.enabled = true;   
+            }
+        }
+    }    
+
+    public void DamagePlayer(int Hurt, Vector3 direction)
+    {
+        if (immundedTime <= 0)
+        {
+            currentHealth -= Hurt;
+
+            if (currentHealth <= 0)
+            {
+                //Respawn(); Need a script for this?
+            }
+            else
+            {
+
+                immundedTime = immuned;
+                modelRenderer1.enabled = false;
+                modelRenderer2.enabled = false;
+
+                blinkTime = blink;
+            }
+        }
     }
+
+    public void Alive() 
+    {
+
+    }
+
+    public void Dead()
+    {
+        if (isDead == true)
+        {
+            respawnTarget.gameObject.SetActive(false);
+            //FindObjectOfType<SoundEffects>().DeathSound(); Do they need new scripts, also are the namings correct?
+            //FindObjectOfType<GameManager>().CameraAfterDeath();
+        }
+    }
+
+    public void Respawn()// New scripts?
+    {
+        isDead = true;
+        FindObjectOfType<GameManager>().EndGame();
+        Dead();
+    }
+
+    public void CheckPoint(Vector3 newLocation)
+    {
+        respawnLocation = newLocation;
+    }
+
+    public void CurrentSpawnPoint(Vector3 newSpawn)
+    {
+        respawnLocation = newSpawn;
+    }
+
+    public void SelectedContinue()
+    {
+        isDead = false;
+        respawnTarget.transform.position = respawnLocation;
+        currentHealth = maxHealth;
+ //     Alive();   ??
+    }
+
+
 }
 
-
-//Testting
